a8f0ffb146d18fff86945dba28d289b577c916fd
EddahK <wangu.kin@gmail.com>
2022-12-21 06:01:35 -0800

    simple shell alx project

--- parser.c
+++ parser.c
@@ -1,17 +1,18 @@
 #include "shell.h"
 
-/**
- * Parsing, syntax analysis, or syntactic analysis is the process of analyzing a string of symbols, 
+/*
+ * Definition: Parsing, syntax analysis, or syntactic analysis
+ * is the process of analyzing a string of symbols,
  * either in natural language, computer languages or data structures
-*/
+ */
 
 /**
- * is_cmd - determines if a file is an executable command
- * @info: the info struct
- * @path: path to the file
- *
- * Return: 1 if true, 0 otherwise
- */
+  * is_cmd - determines if a file is an executable command
+  * @info: the info struct
+  * @path: path to the file
+  *
+  * Return: 1 if true, 0 otherwise
+  */
 int is_cmd(info_t *info, char *path)
 {
 	struct stat st;
